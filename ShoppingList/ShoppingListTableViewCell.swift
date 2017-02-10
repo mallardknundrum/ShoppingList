cb6fd3a4334c0cbbd232fa1856159d4ce364399e
Jeremiah Hawks <mallardknundrum@gmail.com>
2017-02-10 10:21:43 -0700

    custom cell file complete

---
+++ ShoppingList/ShoppingListTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ShoppingListButtonTableViewCell.swift
+//  ShoppingList
+//
+//  Created by Jeremiah Hawks on 2/10/17.
+//  Copyright © 2017 Jeremiah Hawks. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var isCompleteButton: UIButton!
+    
+    weak var delegate: ItemButtonTableViewCellDelegate?
+    
+    var item: Item? {
+        didSet {
+            
+        }
+    }
+   
+    @IBAction func isCompleteButtonTapped(_ sender: Any) {
+        delegate?.isCompleteButtonTapped(sender: self)
+    }
+    
+    func updateViews() {
+        guard let item = item else { return itemName.text = "" }
+        itemName.text = item.name
+        if item.isComplete {
+            isCompleteButton.setImage(#imageLiteral(resourceName: "complete"), for: .normal)
+        } else {
+            isCompleteButton.setImage(#imageLiteral(resourceName: "incomplete"), for: .normal)
+        }
+    }
+
+}
+
+protocol ItemButtonTableViewCellDelegate: class {
+    func isCompleteButtonTapped(sender: ShoppingListTableViewCell)
+}
--- ShoppingList/ShoppingListTableViewController.swift
+++ ShoppingList/ShoppingListTableViewController.swift
@@ -22,7 +22,7 @@ class ShoppingListTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: <#"reuseIdentifier"#>, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingListCell", for: indexPath)
         
         return cell
     }
