cc3de054f09552802ca1ee249aa1cf7986b21380
Jeremiah Hawks <mallardknundrum@gmail.com>
2017-02-10 09:54:43 -0700

    ItemController mostly finished.

---
+++ ShoppingList/ItemController.swift
@@ -0,0 +1,54 @@
+//
+//  ItemController.swift
+//  ShoppingList
+//
+//  Created by Jeremiah Hawks on 2/10/17.
+//  Copyright © 2017 Jeremiah Hawks. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ItemController {
+    
+    // MARK: - Singleton
+    static let shared = ItemController()
+    
+    // MARK: - Create
+    func createItem(withName name: String) {
+        let _ = Item(name: name)
+        saveToPersistentStore()
+    }
+    
+    // MARK: - Read
+    var items: [Item] {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        let moc = Stack.context
+        do {
+            let items = try moc.fetch(request)
+            return items
+        } catch {
+            return []
+        }
+    }
+    
+    // MARK: - Update
+    
+    
+    // MARK: - Delete
+    func deleteItem(item: Item) {
+        let moc = Stack.context
+        moc.delete(item)
+        saveToPersistentStore()
+    }
+    
+    // MARK: - Persistant Storage
+    func saveToPersistentStore() {
+        let moc = Stack.context
+        do {
+            try moc.save()
+        } catch {
+            print("Unable to save to persistant store: \(error)")
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListButtonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListButtonTableViewCell.swift
+//  ShoppingList
+//
+//  Created by Jeremiah Hawks on 2/10/17.
+//  Copyright © 2017 Jeremiah Hawks. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
